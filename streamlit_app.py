42c6d0df9555cca623151a25baf7abc3fac3376e
IRONMAN183 <mnehanth@gmail.com>
2025-01-11 14:07:26 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,11 +42,11 @@ if ingredients_list:
         # Get the corresponding SEARCH_ON value for the selected fruit
         if fruit_chosen in pd_df['FRUIT_NAME'].values:
             search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-            st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
+            #st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
             
             # Fetch and display nutrition information
             st.subheader(f"{fruit_chosen} Nutrition Information")
-            smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}")
+            smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/")
             if smoothiefroot_response.status_code == 200:
                 st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
             else:
